a54ec7a1d70127290c89be57ddc9fbd82e992bc5
Ra1ntop <man111play@gmail.com>
2024-04-30 10:03:02 +0200

    Unnecessary files have been deleted

--- project/src/app/about-page/about-page.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from "@angular/router";
-import {AboutPageComponent} from "./about-page.component";
-
-export const ABOUT_PAGE_ROUTES: Routes = [
-  {
-    path: '',
-    component: AboutPageComponent
-  },
-]

82730069eddc59c85bb7b47a114d9902bb11670f
Ra1ntop <man111play@gmail.com>
2024-04-30 09:49:26 +0200

    The file structure has been redesigned. Configured the router. Created the home component

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,4 +1,25 @@
 import { Routes } from '@angular/router';
-import { WellcomeComponent} from "./wellcome/wellcome.component";
 
-export const routes: Routes = [];
+/**
+ * Настроил роуты, по дефолту будут перехлдить на home
+ */
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',   //по дефолту во всех случаях если нету такого роута на который хочешь перейти
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    pathMatch: 'prefix',
+    loadChildren: () => import('./pages/home/home.routes').then(r => r.HOME_ROUTES)
+  },
+  {
+    path: 'about',
+    pathMatch: 'prefix',
+    loadChildren: () => import('./pages/about-page/about-page.routes').then(r => r.ABOUT_PAGE_ROUTES)
+  },
+
+
+
+];
